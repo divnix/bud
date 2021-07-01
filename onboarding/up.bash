f500617c1f8038767ab5e72167bf34778c686eb7
David Arnold <dgx.arnold@gmail.com>
2021-07-01 15:56:17 -0500

    fix: onboarding/up - teach digga's reverse DNS folder names
    This also obsoletes the need for the `./up` folder since ./hosts
    accepts arbitrarily nested subfolders with a `./default.nix` in it.

--- onboarding/up.bash
+++ onboarding/up.bash
@@ -2,29 +2,30 @@
 
 # ---------------------------------------------------
 synopsis="up"
-help="Generate $FLAKEROOT/hosts/up-$HOST.nix"
+help="Generate $FLKROOT/hosts/up/$HOST.nix"
 description="""
 """
 # ---------------------------------------------------
 
 cmd () {
-  mkdir -p "$FLKROOT/up"
+  targetdir="$FLKROOT/hosts/${HOST//\./\/}"
+  mkdir -p "$targetdir"
 
   # `sudo` is necessary for `btrfs subvolume show`
-  sudo $(which nixos-generate-config) --dir "$FLKROOT/up/$HOST"
+  sudo $(which nixos-generate-config) --dir "$targetdir"
 
   printf "%s\n" \
     "{ suites, ... }:" \
     "{" \
     "  imports = [" \
-    "    ../up/$HOST/configuration.nix" \
+    "    ./configuration.nix" \
     "  ] ++ suites.core;" \
     "" \
     "  flk.enable = true;" \
     "  flk.localFlakeClone = \"$FLKROOT\";" \
-    "}" > "$FLKROOT/hosts/up-$HOST.nix"
+    "}" > "$targetdir/default.nix"
 
-  git add -f \
-    "$FLKROOT/up/$HOST" \
-    "$FLKROOT/hosts/up-$HOST.nix"
+  sudo chown $(id -u):$(id -g) -R "$targetdir"
+
+  git add -f "$targetdir"
 }
