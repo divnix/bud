f6ed7e90fe66307ebc77dbc4cbb3f379b0d2b277
GTrunSec <gtrunsec@hardenedlinux.org>
2021-07-18 11:34:29 -0700

    fix rebuild

--- scripts/hosts-rebuild.bash
+++ scripts/hosts-rebuild.bash
@@ -2,5 +2,10 @@
 
 HOST="${1:-$HOST}"
 
-attr="$FLAKEROOT#\"$HOST\""
-nixos-rebuild --flake "$attr" "${@:2}"
+attr="$FLAKEROOT#$HOST"
+if [ -x /run/wrappers/bin/sudo ]; then
+	export PATH=/run/wrappers/bin:$PATH
+	sudo nixos-rebuild --flake "$attr" "${@:2}"
+else
+	nixos-rebuild --flake "$attr" "${@:2}"
+fi
