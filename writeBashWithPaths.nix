be8ed3fc477b2e7fef2a9396aad1371c2dc15480
GTrunSec <gtrunsec@hardenedlinux.org>
2021-11-11 02:04:20 -0800

    add python script support

--- writeBashWithPaths.nix
+++ writeBashWithPaths.nix
@@ -1,7 +1,27 @@
 { pkgs, lib }:
 paths: extra: script:
+let
+  whichScript = s: lib.hasSuffix s (builtins.baseNameOf script);
+  PYTHONPATH =
+    lib.concatStringsSep ":" (map
+      (f:
+        let
+          pythonVersion = lib.elemAt
+            (builtins.filter builtins.isString
+              (builtins.split "-" "${f}")) 1;
+        in
+        "${f}/lib/${pythonVersion}/site-packages")
+      paths);
+in
 pkgs.writers.writeBash (builtins.baseNameOf script) ''
   export PATH="${lib.makeBinPath paths}"
+  export PYTHONPATH="${PYTHONPATH}"
   ${extra}
+
+  ${lib.optionalString (whichScript "bash") ''
   source ${script}
+  ''}
+  ${lib.optionalString (whichScript "py") ''
+  python ${script}
+  ''}
 ''
