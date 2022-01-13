563f1c680d48ec9cbbec65c7637b30ea04045a18
GTrunSec <gtrunsec@hardenedlinux.org>
2022-01-13 13:36:07 -0800

    add preScript option for script

--- lib/runner.nix
+++ lib/runner.nix
@@ -1,18 +1,20 @@
 { lib, pkgs, ... }:
-paths: name: script:
+paths: preScript: script:
 let
   isPython = script: lib.hasSuffix ".py" script;
   isBashOrsh = script: lib.hasSuffix ".bash" script || lib.hasSuffix ".sh" script;
 
-  run-bash = pkgs.writers.writeBash "${name}-bash" ''
+  run-bash = pkgs.writers.writeBash "${builtins.baseNameOf script}" ''
     export PATH="${lib.makeBinPath paths}"
+    ${preScript}
     source ${script}
   '';
 
-  run-python = pkgs.writers.writePython3 "${name}-python"
+  run-python = pkgs.writers.writePython3 "${builtins.baseNameOf script}"
     {
       libraries = paths;
     } ''
+    ${preScript}
     ${pkgs.lib.fileContents script}
   '';
 
