e3aec72919f8c8b5106bf7ab65849b2038a2b60b
GTrunSec <gtrunsec@hardenedlinux.org>
2022-01-13 13:43:11 -0800

    runner: add haskell

--- lib/runner.nix
+++ lib/runner.nix
@@ -2,6 +2,7 @@
 paths: preScript: script:
 let
   isPython = script: lib.hasSuffix ".py" script;
+  isHaskell = script: lib.hasSuffix ".hs" script;
   isBashOrsh = script: lib.hasSuffix ".bash" script || lib.hasSuffix ".sh" script;
 
   run-bash = pkgs.writers.writeBash "${builtins.baseNameOf script}" ''
@@ -18,8 +19,16 @@ let
     ${pkgs.lib.fileContents script}
   '';
 
+  run-haskell = pkgs.writers.writeHaskell "${builtins.baseNameOf script}"
+    {
+      libraries = paths;
+    } ''
+    ${preScript}
+    ${pkgs.lib.fileContents script}
+  '';
+
   runner = if (isPython script) then run-python else
-  if (isBashOrsh script) then run-bash else
-  run-bash;
+  if (isBashOrsh script) then run-bash else if (isHaskell script) then
+    run-haskell else run-bash;
 in
 runner
